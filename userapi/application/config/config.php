69b3e4c9e72c7e37d4137b37540a22b5dea51faa
test <aa@tmail.ws>
2017-02-25 20:30:54 +0000

    Modified base_url

--- userapi/application/config/config.php
+++ userapi/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://codeigniter-testaa.c9users.io/ciuserapi/';
+$config['base_url'] = 'https://codeigniter-testaa.c9users.io/userapi/';
 
 /*
 |--------------------------------------------------------------------------
