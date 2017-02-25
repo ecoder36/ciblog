2f179a8c77085d79fecc47be74734d5f00f40c7c
test <aa@tmail.ws>
2017-02-25 20:19:25 +0000

    Added userapi

---
+++ userapi/application/views/user/test/first.php
@@ -0,0 +1,14 @@
+
+
+
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>first page</h1>
+			</div>
+			<p>welcome............</p>
+		</div>
+	</div><!-- .row -->
+</div><!-- .container -->
